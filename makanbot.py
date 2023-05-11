7469ba8921a647ad174d1194b227d26533dbc1d0
Joel Kong <joelkong2001@gmail.com>
2023-05-11 13:10:22 +0800

    feat: created start function

--- makanbot.py
+++ makanbot.py
@@ -1,5 +1,20 @@
-import telegram
+from telegram import Bot, Update, ReplyKeyboardMarkup, KeyboardButton
 import requests
-from dotenv import load_dotenv
+import dotenv
+import os
 
-load_dotenv()
+dotenv.load_dotenv()
+foursquare_api_key = os.environ['FOURSQUARE_API_KEY']
+bot = os.environ['BOT_API_KEY']
+
+def start(update):
+    bot.sendMessage(update.message.chat_id, "Welcome to the MakanBot! I can help you find nearby restaurants around you.")
+
+    bot.sendMessage(
+        update.message.chat_id,
+        "Send me your location so i can find nearby restaurants around you.",
+        reply_markup=ReplyKeyboardMarkup([
+            KeyboardButton("Send Location", request_location=True)
+        ]),
+    )
+
